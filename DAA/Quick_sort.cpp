a04e8887c0bd862b83307b63ab6e9f0e21981150
theyashjadhav <113454646+theyashjadhav@users.noreply.github.com>
2023-08-11 09:54:16 +0530

    Update

--- DAA/Quick_sort.cpp
+++ DAA/Quick_sort.cpp
@@ -1,16 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int partition(int arr[], int l, int r, bool f, int &steps) {
+int partition(int arr[], int l, int r, bool f, int &steps)
+{
     int iPivot = (f ? r : (l + rand() % (r - l + 1)));
     swap(arr[iPivot], arr[r]);
     int pivot = arr[r];
     int i = l - 1;
-    for (int j = l; j < r; j++) {
-        if (arr[j] < pivot) {
+    for (int j = l; j < r; j++)
+    {
+        if (arr[j] < pivot)
+        {
             i++;
             swap(arr[i], arr[j]);
-            steps++; 
+            steps++;
         }
     }
     swap(arr[i + 1], arr[r]);
@@ -18,12 +21,15 @@ int partition(int arr[], int l, int r, bool f, int &steps) {
     return (i + 1);
 }
 
-void quicksort(int arr[], int l, int r, bool f, int &steps) {
-    if (l < r) {
+void quicksort(int arr[], int l, int r, bool f, int &steps)
+{
+    if (l < r)
+    {
         int iPivot = partition(arr, l, r, f, steps);
         cout << "Pivot: " << arr[iPivot] << " ";
         cout << "Sorted Array: ";
-        for (int i = l; i <= r; i++) {
+        for (int i = l; i <= r; i++)
+        {
             cout << arr[i] << " ";
         }
         cout << endl;
@@ -32,49 +38,54 @@ void quicksort(int arr[], int l, int r, bool f, int &steps) {
     }
 }
 
-int main() {
+int main()
+{
     int n;
     cout << "Enter the size of the array: ";
     cin >> n;
 
-    int* arr = new int[n];
-    int* arr1 = new int[n];
-    
+    int *arr = new int[n];
+    int *arr1 = new int[n];
+
     cout << "Enter " << n << " elements: ";
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++)
+    {
         cin >> arr[i];
         arr1[i] = arr[i];
     }
 
     cout << "Original array: ";
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++)
+    {
         cout << arr[i] << " ";
     }
     cout << "\n\n";
-    
-    int steps = 0; 
+
+    int steps = 0;
     auto start = std::chrono::high_resolution_clock::now();
     quicksort(arr, 0, n - 1, 0, steps);
     auto stop = std::chrono::high_resolution_clock::now();
     auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(stop - start);
     cout << "Time taken: " << duration.count() << " ms" << endl;
     cout << "Sorted array with pivot as the random element: ";
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++)
+    {
         cout << arr[i] << " ";
     }
-    cout <<"\nNumber of steps: " << steps << "\n\n";
-    
-    steps = 0; 
+    cout << "\nNumber of steps: " << steps << "\n\n";
+
+    steps = 0;
     start = std::chrono::high_resolution_clock::now();
     quicksort(arr1, 0, n - 1, 1, steps);
     stop = std::chrono::high_resolution_clock::now();
     duration = std::chrono::duration_cast<std::chrono::milliseconds>(stop - start);
     cout << "Time taken: " << duration.count() << " ms" << endl;
     cout << "Sorted array with rightmost pivot: ";
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++)
+    {
         cout << arr1[i] << " ";
     }
     cout << "\nNumber of steps: " << steps << "\n";
-    
+
     return 0;
 }
