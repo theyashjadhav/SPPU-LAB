7cdd40f7e8eadd749dd04c3e6bd0a8a8ea40db02
theyashjadhav <113454646+theyashjadhav@users.noreply.github.com>
2023-11-05 15:35:01 +0530

    Update

---
+++ DAA/Exrtra/02_pract.cpp
@@ -0,0 +1,111 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Function to perform Merge Sort
+void merge(vector<int>& arr, int l, int m, int r) {
+    int n1 = m - l + 1;
+    int n2 = r - m;
+    
+    vector<int> L(n1), R(n2);
+    
+    for (int i = 0; i < n1; i++)
+        L[i] = arr[l + i];
+    
+    for (int j = 0; j < n2; j++)
+        R[j] = arr[m + 1 + j];
+    
+    int i = 0, j = 0, k = l;
+    
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) {
+            arr[k] = L[i];
+            i++;
+        }
+        else {
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+    
+    while (i < n1) {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+    
+    while (j < n2) {
+        arr[k] = R[j];
+        j++;
+        k++;
+    }
+}
+
+void mergeSort(vector<int>& arr, int l, int r) {
+    if (l < r) {
+        int m = l + (r - l) / 2;
+        
+        mergeSort(arr, l, m);
+        mergeSort(arr, m + 1, r);
+        
+        merge(arr, l, m, r);
+    }
+}
+
+// Function to perform Insertion Sort
+void insertionSort(vector<int>& arr) {
+    int n = arr.size();
+    
+    for (int i = 1; i < n; i++) {
+        int curret = arr[i];
+        int j = i - 1;
+        
+        while (j >= 0 && arr[j] > curret) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        
+        arr[j + 1] = curret;
+    }
+}
+
+int main() {
+    vector<int> numbers;
+    int num;
+    
+    ifstream inputFile("input.txt");
+    
+    if (!inputFile) {
+        cout << "Error opening file!";
+        return 1;
+    }
+    
+    while (inputFile >> num) {
+        numbers.push_back(num);
+    }
+    
+    inputFile.close();
+    
+    // Applying Merge Sort
+    mergeSort(numbers, 0, numbers.size() - 1);
+    
+    // Applying Insertion Sort
+    insertionSort(numbers);
+    
+    ofstream outputFile("output.txt");
+    
+    if (!outputFile) {
+        cout << "Error creating output file!";
+        return 1;
+    }
+    
+    for (int num : numbers) {
+        outputFile << num << " ";
+    }
+    
+    outputFile.close();
+    
+    cout << "Sorting completed and saved to output.txt\n";
+    
+    return 0;
+}
---
+++ DAA/Exrtra/Quick_sort.cpp
@@ -0,0 +1,91 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int partition(int arr[], int l, int r, bool f, int &steps)
+{
+    int iPivot = (f ? r : (l + rand() % (r - l + 1)));
+    swap(arr[iPivot], arr[r]);
+    int pivot = arr[r];
+    int i = l - 1;
+    for (int j = l; j < r; j++)
+    {
+        if (arr[j] < pivot)
+        {
+            i++;
+            swap(arr[i], arr[j]);
+            steps++;
+        }
+    }
+    swap(arr[i + 1], arr[r]);
+    steps++;
+    return (i + 1);
+}
+
+void quicksort(int arr[], int l, int r, bool f, int &steps)
+{
+    if (l < r)
+    {
+        int iPivot = partition(arr, l, r, f, steps);
+        cout << "Pivot: " << arr[iPivot] << " ";
+        cout << "Sorted Array: ";
+        for (int i = l; i <= r; i++)
+        {
+            cout << arr[i] << " ";
+        }
+        cout << endl;
+        quicksort(arr, l, iPivot - 1, f, steps);
+        quicksort(arr, iPivot + 1, r, f, steps);
+    }
+}
+
+int main()
+{
+    int n;
+    cout << "Enter the size of the array: ";
+    cin >> n;
+
+    int *arr = new int[n];
+    int *arr1 = new int[n];
+
+    cout << "Enter " << n << " elements: ";
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+        arr1[i] = arr[i];
+    }
+
+    cout << "Original array: ";
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << "\n\n";
+
+    int steps = 0;
+    auto start = std::chrono::high_resolution_clock::now();
+    quicksort(arr, 0, n - 1, 0, steps);
+    auto stop = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(stop - start);
+    cout << "Time taken: " << duration.count() << " ms" << endl;
+    cout << "Sorted array with pivot as the random element: ";
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << "\nNumber of steps: " << steps << "\n\n";
+
+    steps = 0;
+    start = std::chrono::high_resolution_clock::now();
+    quicksort(arr1, 0, n - 1, 1, steps);
+    stop = std::chrono::high_resolution_clock::now();
+    duration = std::chrono::duration_cast<std::chrono::milliseconds>(stop - start);
+    cout << "Time taken: " << duration.count() << " ms" << endl;
+    cout << "Sorted array with rightmost pivot: ";
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr1[i] << " ";
+    }
+    cout << "\nNumber of steps: " << steps << "\n";
+
+    return 0;
+}
